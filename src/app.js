4f2a7d6a3bd380c5602b254171f4e59bb7fa6254
DefineXX <keaikim21@naver.com>
2025-03-15 16:29:09 +0900

    feat: Task에 대한 Check 기능 구현

--- src/app.js
+++ src/app.js
@@ -1,27 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
   const taskInput = document.querySelector('.add-task-input');
   const addTaskButton = document.querySelector('.add-task-button');
-  const taskList = document.querySelector('.task-list');
+  const toDoList = document.getElementById('to-do-list');
+  const doneList = document.getElementById('done-list');
 
   const addTask = () => {
     const taskValue = taskInput.value.trim();
-    if (taskValue === '') alert('Please Enter Your Task!');
+    if (taskValue === '') {
+      alert('Please Enter Your Task!');
+      return;
+    }
 
     const taskItem = document.createElement('li');
-    taskItem.classList.add('task-item', 'to-do');
+    taskItem.classList.add('task-item', 'to-do-item');
+
+    const checkboxContainer = document.createElement('div');
+    checkboxContainer.classList.add('checkbox-container');
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.classList.add('task-checkbox');
 
+    checkboxContainer.appendChild(checkbox);
+
     const taskText = document.createElement('span');
     taskText.classList.add('task-text');
     taskText.textContent = taskValue;
 
-    taskItem.appendChild(checkbox);
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        taskItem.classList.remove('to-do-item');
+        taskItem.classList.add('done-item');
+        taskText.classList.add('done-task');
+
+        const checkIcon = document.createElement('img');
+        checkIcon.src = 'public/check.svg';
+        checkIcon.alt = 'Check Icon';
+        checkIcon.classList.add('check-icon');
+        checkboxContainer.appendChild(checkIcon);
+
+        doneList.appendChild(taskItem);
+      } else {
+        taskItem.classList.remove('done-item');
+        taskItem.classList.add('to-do-item');
+        taskText.classList.remove('done-text');
+        toDoList.appendChild(taskItem);
+      }
+    });
+
+    taskItem.appendChild(checkboxContainer);
     taskItem.appendChild(taskText);
 
-    taskList.appendChild(taskItem);
+    toDoList.appendChild(taskItem);
 
     taskInput.value = '';
   };
